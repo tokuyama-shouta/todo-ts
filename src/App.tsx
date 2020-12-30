94270e21a32f0be91bb3ed07eacb2c8b0ffcf015
tokuyama shouta <toku1031ts@gmail.com>
2020-12-30 15:26:25 +0900

    input materialui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <div className="todoContainer">
+      <TextField
+          required
+          id="outlined-required"
+          label="Todo Input"
+          defaultValue=""
+          className="textField"
+        />
       <Button variant="contained" color="primary">
-        Primary
+        追加
       </Button>
+      </div>
     </div>
 
   );
