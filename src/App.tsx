05072fdbad2901a86600320ab7380ca19dd540b9
tokuyama shouta <toku1031ts@gmail.com>
2021-04-23 20:48:10 +0900

    データ型”

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import React,{useState} from 'react';
 import './App.css';
 
+const name = "hello";
 
+let nameChange = "hello";
+nameChange ="hello2";
 
+let username = "Hello";
+let dummyNum = 2;
+let bool: boolean = true;
+
+let array = [true, false, true];
+let array2 = [0,1,"hello"];
+
+interface NAME {
+  first: string;
+  last: string;
+}
+
+let nameObj: NAME = {first: "Yamada", last: "Taro"};
+
+const func1 = (x: number, y: number):number => {
+  return x + y;
+}
 
 const App: React.FC = () => {
   
