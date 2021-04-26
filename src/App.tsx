032eab6ddd31b386eafca77eeda11157c01ae4a5
tokuyama shouta <toku1031ts@gmail.com>
2021-04-26 20:49:52 +0900

    enum

--- src/App.tsx
+++ src/App.tsx
@@ -88,6 +88,27 @@ const SPORTS = {
 let keySports: keyof typeof SPORTS;
 keySports = "soccer";
 
+// enum
+enum OS {
+  Windows,
+  Mac,
+  Linux,
+}
+
+interface PC {
+  id: number,
+  OSType: OS;
+}
+const PC1: PC = {
+  id:1,
+  OSType: OS.Windows
+}
+const PC2: PC = {
+  id:2,
+  OSType: OS.Mac,
+}
+
+
 const App: React.FC = () => {
   
 
