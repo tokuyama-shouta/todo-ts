78f60c9bffbb674acd470d45deae89cccc06e851
tokuyama shouta <toku1031ts@gmail.com>
2021-05-01 11:28:57 +0900

    event

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -11,12 +11,18 @@ interface UserData {
 
 const TestComponent: React.FC<Props> = (props) => {
   const [count, setCount] = useState<number | null>(0);
-  const [user, setUser] = useState<UserData>({id:1, name: "pk"})
+  const [user, setUser] = useState<UserData>({id:1, name: "pk"});
+  const [inputData, setInputData] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => 
+  setInputData(e.target.value);
   
   return (
     <div>
       <h1>{props.text}</h1>
       <h1>{count}</h1>
+      <input type="text" value={inputData} onChange={handleInputChange}/>
+      <h1>{inputData}</h1>
     </div>
   )
 }
